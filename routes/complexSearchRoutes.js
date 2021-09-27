398175bc37bd472099ae1bbb81975cd794db5664
Beers15 <alexbeerz@yahoo.com>
2021-09-27 12:25:35 -0700

    Change shape of response returned from GET /recipes/complex-search.

--- routes/complexSearchRoutes.js
+++ routes/complexSearchRoutes.js
@@ -2,14 +2,27 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-const getRecipes = async (req, res) => {
-  
+const getRecipes = async (req, res) => { 
   try {
     const response = await axios.get(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_KEY}&cuisine=${req.query.cuisine}&diet=${req.query.diet}&intolerences=${req.query.intolerances}&equipment=${req.query.equipment}&type=${req.query.type}&sort=meta-score&addRecipeInformation=true&number=6`
     );
+    const results = response.data.results;
 
-    res.send(response.data);
+    for(let result of results) {
+      let resultSteps = [];
+      let resultEquipment = [];
+      for(let step of result.analyzedInstructions[0].steps) {
+        resultSteps.push(step.step);
+      }
+      for(let step of result.analyzedInstructions[0].steps) {
+        step.equipment[0] ? resultEquipment.push(step.equipment[0].name) : resultEquipment.push('none');
+      }
+      result.steps = resultSteps;
+      result.equipment = resultEquipment;
+      delete result.analyzedInstructions;
+    }
+    res.send(results);
   } catch (err) {
     console.log(err)
     res.status(404).send(err);
