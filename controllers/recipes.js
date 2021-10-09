6aabaed536ee8d22169956f8338f102d478408e9
Beers15 <alexbeerz@yahoo.com>
2021-10-09 11:06:27 -0700

    Redo layout with flexbox for Profile and AskChef Page.

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const Recipe = require('../models/Recipe');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
+const testdata = require('../testData');
 
 const client = jwksClient({
   jwksUri: 'https://dev-qttzuf0f.us.auth0.com/.well-known/jwks.json',
@@ -108,28 +109,30 @@ module.exports.updateRecipe = async (req, res) => {
 };
 
 module.exports.getRecipesByComplexSearch = async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_KEY}&cuisine=${req.query.cuisine}&diet=${req.query.diet}&intolerences=${req.query.intolerances}&equipment=${req.query.equipment}&type=${req.query.type}&sort=meta-score&addRecipeInformation=true&number=6`
-    );
-    const results = response.data.results;
-    console.log(results);
-    for(let result of results) {
-      let resultSteps = [];
-      let resultEquipment = [];
-      for(let step of result.analyzedInstructions[0].steps) {
-        resultSteps.push(step.step);
-      }
-      for(let step of result.analyzedInstructions[0].steps) {
-        step.equipment[0] ? resultEquipment.push(step.equipment[0].name) : resultEquipment.push('none');
-      }
-      result.steps = resultSteps;
-      result.equipment = resultEquipment;
-      delete result.analyzedInstructions;
-    }
-    res.send(results);
-  } catch (err) {
-    console.log(err);
-    res.status(404).send(err);
-  }
+  const results = testdata;
+  // try {
+  //   const response = await axios.get(
+  //     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_KEY}&cuisine=${req.query.cuisine}&diet=${req.query.diet}&intolerences=${req.query.intolerances}&equipment=${req.query.equipment}&type=${req.query.type}&sort=meta-score&addRecipeInformation=true&number=6`
+  //   );
+  //   const results = response.data.results;
+  //   console.log(results);
+  //   for(let result of results) {
+  //     let resultSteps = [];
+  //     let resultEquipment = [];
+  //     for(let step of result.analyzedInstructions[0].steps) {
+  //       resultSteps.push(step.step);
+  //     }
+  //     for(let step of result.analyzedInstructions[0].steps) {
+  //       step.equipment[0] ? resultEquipment.push(step.equipment[0].name) : resultEquipment.push('none');
+  //     }
+  //     result.steps = resultSteps;
+  //     result.equipment = resultEquipment;
+  //     delete result.analyzedInstructions;
+  //   }
+
+  res.send(results);
+  // } catch (err) {
+  //   console.log(err);
+  //   res.status(404).send(err);
+  // }
 };
